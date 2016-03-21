dbeaa239eb804f228efb0c2493555805df43cb05
elsehow <yes@cosmopol.is>
2016-03-21 12:43:40 -0600

    barebones workspace

---
+++ index.js
@@ -0,0 +1,10 @@
+import swarmlog from 'swarmlog'
+import sodium from 'chlroide/browser'
+
+const idSwarm = (opts) => {
+  opts.sodium = sodium
+  opts.valueEncoding = 'json'
+  let log = 
+}
+
+export default idSwarm
